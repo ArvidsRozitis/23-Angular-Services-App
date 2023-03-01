8f1a6c4da488d7c93bd55439f55a19e7ebf25063
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-03-01 15:24:02 +0200

    Created Simple Services Test App

---
+++ services-app/src/app/services/services.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.scss'],
+})
+export class ServicesComponent {
+  handleSelection = (
+    service: { name: string; price: number; selected: boolean }
+  ) => {
+    if (!service.selected) {
+      this.btnStyle = 'btn__active';
+      this.priceToPay += service.price;
+      service.selected = true;
+    } else {
+      this.priceToPay -= service.price;
+      service.selected = false;
+      this.btnStyle = 'btn';
+    }
+  };
+  displayPrice = (number: number) => {
+    return number.toFixed(2);
+  };
+  services = [
+    { name: 'Web Developmenr', price: 300.5, selected: false, },
+    { name: 'Design', price: 400, selected: false },
+    { name: 'Integration', price: 250.9, selected: false },
+    { name: 'Training', price: 220.01, selected: false },
+  ];
+
+  btnStyle= 'btn';
+  priceToPay = 0;
+}
